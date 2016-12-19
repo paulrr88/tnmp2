dcf03a58eef1ca432f9add9e74cd58a5a304fff6
paulrr88 <paul@Pauls-iMac.home>
2016-12-19 16:36:48 +0000

    Content sorting, docs added, started about page

--- about.php
+++ about.php
@@ -1,12 +1,111 @@
-<!--$page-title Home-->
-<!--$description nil TBD-->
-<!--$dir nil-->
-<!--$jquery nil-->
-<!--$keywords TBD-->
-<!--$tablescript nil-->
-<!--$unitegallery nil-->
 
-<!--@import opening-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Home &mdash; The New Montessori Pre-school</title>
+  <meta name="description" content="nil TBD">
+  <meta name="keywords" content="TBD, TBD">
+  <meta name="author" content="St. Andrew's Church, Ferring">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSS & JavaScript
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css"><!-- reset file do not change -->
+  <link rel="stylesheet" href="css/skeleton.css"><!-- framework CSS -->
+  <link rel="stylesheet" href="css/style.css?v=1"><!--my additions-->
+
+  
+  
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+
+<body>
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <!--Container for ALL content, everything inside this--><div class="container">
+
+    <!--Row begins--><div class="row">
+
+      <!--Start of top information area--><div class="twelve columns" id="top">
+        <ul class="nostyle">
+          <li><a href="sitemap">Sitemap</a></li>
+          <li><a href="docs/AEDnotice.pdf"><img src="images/aed.jpg" /></a></li>
+        </ul>
+      </div><!--End of top information area-->
+
+    </div><!--Row ends-->
+
+
+    <!--Row begins--><div class="row">
+
+      <!--top link for bookmarks on long pages--><a name="top"></a>
+
+      <!--Header begin--><div class="twelve columns" id="header">
+
+        <div class="twelve columns">
+          <div id="header-logo"></div>
+
+          <div id="header-title-container">
+            <h1 id="header-title">The New Montessori Pre-school</h1>
+          </div>
+        </div>
+
+      </div><!--Header ends-->
+
+    </div><!--Row ends-->
+
+
+    <!--Row begins--><div id="row">
+
+      <!--Navigation area begins--><div class="twelve columns" id="nav">
+        <!--Taglines at bottom of header-->
+
+        <div class="three columns" id="tagline-phone">
+          <p>01903 535412</p>
+        </div>
+        <div class="six columns" id="tagline-email">
+          <p>info@thenewmontessoripreschool.org.uk</p>
+        </div>
+        <div class="three columns" id="tagline-phone-mobile">
+          <p>07939 264728</p>
+        </div>
+
+        <!--<div class="seven columns">
+          <p id="left">In the Anglican Diocese of Chichester, and Worthing Deanery</p>
+        </div>
+        <div class="five columns">
+          <p id="right">Church Lane, Ferring, West Sussex, BN12 5HN</p>
+        </div>-->
+        <div class="twelve columns" id="navbuttons">
+          <div id="navbuttons-aligner">
+            <div id="home"><a href="home">Home</a></div>
+            <div id="about">About Us</div>
+            <div id="dates">Fees &amp; Dates</div>
+            <div id="child">My Montessori Child</div>
+            <div id="contact">Contact Us</div>
+            <div id="facebook">Facebook</div>
+          </div>
+        </div>
+      </div><!--Navigation area ends-->
+
+    </div><!--Row ends-->
+
+
+    <!--Row begins--><div class="row">
+
+
+
+      <!--Content begins #content--><div class="twelve columns nomargin" id="content">
 
 
         <ul>
@@ -102,4 +201,44 @@
 
 
 
-<!--@import footer-->
+      </div><!--Content ends-->
+
+    </div><!--Row ends-->
+
+
+    <!--Row begins--><div class="row">
+
+      <!--Footer begins #footer--><div class="twelve columns" id="footer">
+        <p><strong>&copy; 2016 The New Montessori Pre-school | Designed by Paul Robinson</strong></p>
+        <p>Registered charity no. 1086853</p>
+        <a href="http://www.facebook.com/standrewsferring"><img src="images/facebook.png" alt="St. Andrew's Church Ferring Facebook" /></a>
+
+        <a href=""><img src="images/diocese.png" /></a>
+
+        <br />
+
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="contacts">Contacts</a></li>
+          <li><a href="about">Who's Who</a></li>
+          <li><a href="https://goo.gl/maps/NSJ76SqrDo32">Where to find St. Andrew's</a></li>
+          <li><a href="about/history">History</a></li>
+          <li><a href="services">Services &amp; Dates</a></li>
+          <li><a href="clubs">Clubs &amp; Activities</a></li>
+          <li><a href="clubs/#social">Social Life at St. Andrew's</a></li>
+          <li><a href="clubs/#transport">Church Transport</a></li>
+          <li><a href="photos">Photo Gallery</a></li>
+          <li><a href="stewardship">Stewardship</a></li>
+          <li><a href="links">Links</a></li>
+        </ul>
+
+      </div><!--Footer ends-->
+
+    </div><!--Row ends-->
+
+  </div><!--Container ends-->
+
+<!-- End Document
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html>
