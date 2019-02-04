1f3def8bfbcf335b904f79f5371e74301819126e
paulrr88 <paul@pauls-imac.home>
2019-02-04 15:00:56 +0000

    FAQ added, started customising Perch template

---
+++ faq.php
@@ -0,0 +1,175 @@
+
+
+<?php include('perch/runtime.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Frequently Asked Questions &mdash; The New Montessori Pre-school</title>
+  <meta name="description" content="We try to answer your questions in our FAQ!">
+  <meta name="keywords" content="accredited, active, activity, "all day care", care, charity, childcare, children, classroom, daycare, development, educate, education, evening, flexible, infant, junior, kids, learning, lessons, local, maria, "Maria Montessori", method, montessori, nursery, pre-school, preschool, school, sussex, tarring, toddler, west, worthing, ">
+  <meta name="author" content="The New Montessori Pre-school">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSS & JavaScript
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="../css/normalize.css"><!-- reset file do not change -->
+  <link rel="stylesheet" href="../css/skeleton.css"><!-- framework CSS -->
+  <link rel="stylesheet" href="../css/style.css"><!--my additions-->
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="../images/favicon.png">
+
+</head>
+
+<body>
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <!--Container for ALL content, everything inside this--><div class="container">
+
+    <!--Row begins--><div class="row">
+
+      <!--Start of top information area--><div class="twelve columns" id="top">
+        <ul class="nostyle">
+          <li class="vertical-top"><a href="../sitemap/">Sitemap</a></li>
+          <li><a href="<?php perch_content('Food rating link'); ?>"><img src="../images/food.jpg" width="100" alt="Link to our food hygiene rating" /></a></li>
+          <li><img src="../images/accredited.jpg" width="70" alt="This school is Montessori MEAB Accredited" /></a></li>
+
+        </ul>
+      </div><!--End of top information area-->
+
+    </div><!--Row ends-->
+
+
+    <!--Row begins--><div class="row">
+
+      <!--top link for bookmarks on long pages--><a name="top"></a>
+
+      <!--Header begin--><div class="twelve columns" id="header">
+
+        <div class="twelve columns">
+          <div id="header-logo"></div>
+
+          <div id="header-title-container">
+            <h1 id="header-title">
+              <span id="header-the">The</span>
+              <span id="header-new">New</span>
+              <span id="header-montessori">Montessori</span>
+              <span id="header-preschool">Pre-school</span>
+            </h1>
+          </div>
+        </div>
+
+      </div><!--Header ends-->
+
+    </div><!--Row ends-->
+
+
+    <!--Row begins--><div id="row">
+
+      <!--Navigation area begins--><div class="twelve columns" id="nav">
+        <!--Taglines at bottom of header-->
+
+        <?php include('header-contact.php')?>
+
+        <!--<div class="seven columns">
+          <p id="left">In the Anglican Diocese of Chichester, and Worthing Deanery</p>
+        </div>
+        <div class="five columns">
+          <p id="right">Church Lane, Ferring, West Sussex, BN12 5HN</p>
+        </div>-->
+        <div class="twelve columns" id="navbuttons">
+          <div id="navbuttons-aligner">
+            <a class="nav" href="../"><div id="home">Home</div></a>
+            <a class="nav" href="../about/"><div id="about">About Us</div></a>
+            <a class="nav" href="../datesandfees/"><div id="dates">Dates &amp; Fees</div></a>
+            <a class="nav" href="../mymontessorichild/"><div id="child">My Montessori Child</div></a>
+            <a class="nav" href="../contact/"><div id="contact">Contact Us</div></a>
+            <a class="nav" href="https://www.facebook.com/The-New-Montessori-Pre-School-210642929391/"><div id="facebook">Facebook</div></a>
+          </div>
+        </div>
+      </div><!--Navigation area ends-->
+
+    </div><!--Row ends-->
+
+
+    <!--Row begins--><div class="row">
+
+      <!--Content begins #content--><div class="twelve columns nomargin" id="content">
+
+
+    <!--Row begins--><div class="row">
+
+        <!--Content begins #content--><div class="twelve columns">
+
+        <h1 class="title">The New Montessori Pre-school Frequently Asked Questions</h1>
+        
+        <?php perch_content('FAQ'); ?>
+
+      <br />
+      <br />
+
+    </div><!--Row ends-->
+
+      </div><!--Content ends-->
+
+
+
+    </div><!--Row ends-->
+
+    <div class="row">
+
+
+
+    </div>
+
+
+
+    <!--Row begins--><div class="row">
+
+      <!--Footer begins #footer--><div class="twelve columns" id="footer">
+
+        <br />
+
+        <ul>
+          <li><a href="../">Home</a></li>
+          <li><a href="../about/">What is Montessori?</a></li>
+          <li><a href="../datesandfees/">Dates &amp; Fees</a></li>
+          <li><a href="../contact/">Contact Us</a></li>
+          <li><a href="../mymontessorichild/">My Montessori Child</a></li>
+          <li><a href="../about/method/">The Montessori Method</a></li>
+          <li><a href="../about/history/">History of Montessori and Our Pre-school</a></li>
+          <li><a href="../about/team/">Our Team</a></li>
+          <li><a href="../about/policies/">Our Policies &amp; Documents</a></li>
+          <li><a href="https://www.facebook.com/The-New-Montessori-Pre-School-210642929391/">Facebook</a></li>
+        </ul>
+
+        <p><strong>&copy; <?php echo date('Y'); ?> The New Montessori Pre-school | Designed by <a href="http://www.paulcomputerhelp.co.uk/">Paul Robinson</a></strong><br />
+        Registered charity no. 1086853<br />
+        Company Number 3908382<br />
+        <br />
+        <a href="../perch/">Perch Login</a></p>
+        <br />
+        <br />
+        <br />
+
+      </div><!--Footer ends-->
+
+    </div><!--Row ends-->
+
+  </div><!--Container ends-->
+
+  
+
+<!-- End Document
+–––––––––––––––––––––––––––––––––––––––––––––––––– -->
+</body>
+</html>
+
